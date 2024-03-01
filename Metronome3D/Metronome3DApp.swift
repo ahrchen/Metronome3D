5d44d9ad13347bce37cd8e58f8ce4de14da2835f
Raymond Chen <ahrchen@gmail.com>
2024-03-01 12:18:48 -0800

    Initial Commit

---
+++ Metronome3D/Metronome3DApp.swift
@@ -0,0 +1,21 @@
+//
+//  Metronome3DApp.swift
+//  Metronome3D
+//
+//  Created by Raymond Chen on 3/1/24.
+//
+
+import SwiftUI
+
+@main
+struct Metronome3DApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }.windowStyle(.volumetric)
+
+        ImmersiveSpace(id: "ImmersiveSpace") {
+            ImmersiveView()
+        }
+    }
+}
