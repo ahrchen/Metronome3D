1e618fee11c2b058b4c4c1d1e159367c4555aa20
Raymond Chen <ahrchen@gmail.com>
2024-03-01 16:00:18 -0800

    Add UI Controls

--- Metronome3D/ContentView.swift
+++ Metronome3D/ContentView.swift
@@ -11,22 +11,56 @@ import RealityKitContent
 
 struct ContentView: View {
 
-    @State private var enlarge = false
-
+    @State private var isPlaying = false
+    @State private var bpm: Double = 90.0
+    
     var body: some View {
         VStack {
-            RealityView { content in
-                // Add the initial RealityKit content
-                if let metronomeEntity = try? await Entity(named: "Metronome", in: realityKitContentBundle), let environment = try? await EnvironmentResource(named: "studio") {
-                    
-                    metronomeEntity.components.set(ImageBasedLightComponent(source: .single(environment)))
-                    metronomeEntity.components.set(ImageBasedLightReceiverComponent(imageBasedLight: metronomeEntity))
-                    metronomeEntity.components.set(GroundingShadowComponent(castsShadow: true))
-            
-                    content.add(metronomeEntity)
-                }
+            ZStack(alignment: Alignment(horizontal: .center, vertical: .bottom)) {
+                metronome
+                metronomeControls
             }
         }
+    }
+    
+    var metronome: some View {
+        RealityView { content in
+            // Add the initial RealityKit content
+            if let metronomeEntity = try? await Entity(named: "Metronome", in: realityKitContentBundle), let environment = try? await EnvironmentResource(named: "studio") {
+                
+                metronomeEntity.components.set(ImageBasedLightComponent(source: .single(environment)))
+                metronomeEntity.components.set(ImageBasedLightReceiverComponent(imageBasedLight: metronomeEntity))
+                metronomeEntity.components.set(GroundingShadowComponent(castsShadow: true))
+        
+                content.add(metronomeEntity)
+            }
+        }
+    }
+    
+    var metronomeControls: some View {
+        VStack {
+            Button {
+                Task {
+                    isPlaying.toggle()
+                }
+            } label: {
+                Label(isPlaying ? "Play" : "Pause", systemImage: isPlaying ? "play" : "pause")
+            }
+            .frame(width: 400)
+            .buttonStyle(.borderless)
+            .labelStyle(.iconOnly)
+            .font(.system(size: 80))
+            .glassBackgroundEffect(in: .rect(cornerRadius: 50))
+            
+            VStack {
+                Slider(value: $bpm, in: 40...134, step: 1)
+            }
+            .controlSize(.extraLarge)
+            Text("\(Int(bpm)) BPM")
+                .font(.largeTitle)
+        }
+        .frame(width: 400, height: 500)
+        .offset(y: -250)
     }
 }
 
